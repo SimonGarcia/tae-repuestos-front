58fd5a3e1f68b0bce489b5b085a4e64e66a8cdb9
Andrea Guevara <andrea.guevara@incluit.com>
2021-06-18 13:36:28 -0300

    adding filterPipe in table

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -7,16 +7,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, filterString:string ) {
-      if (value.length === 0 || filterString === '') {
-        return value;
-      }
-      const repuestos = [];
-      for(const repuesto of value){
-        if (repuesto['codigo'] === filterString || repuesto['nombre'] === filterString) {
-          repuestos.push(repuesto);
-        }
-    }
-    return repuestos;
+    //   if (value.length === 0 || filterString === '') {
+    //     return value;
+    //   }
+    //   const repuestos = [];
+    //   for(const repuesto of value){
+    //     if (repuesto['codigo'] === filterString || repuesto['nombre'] === filterString) {
+    //       repuestos.push(repuesto);
+    //     }
+    // }
+    // return repuestos;
+
+    
+    if(!filterString) return value;
+    return value.filter((item:any) => (item.nombre.toLowerCase().indexOf(filterString.toLowerCase()) > -1 || item.codigo.toLowerCase().indexOf(filterString.toLowerCase()) > -1));
   }
 
 }
